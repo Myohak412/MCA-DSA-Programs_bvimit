8a51a9f6eaa36d82e4e6dbcc02e3ec600914313e
Mohak W <mohakwaghmare2002@gmail.com>
2025-09-24 19:12:20 +0530

    solved by own

---
+++ byown.java
@@ -0,0 +1,15 @@
+public class byown {
+    public static void main(String[] args) {
+            int N = 20;
+            int sum =0;
+            for(int i=0; i<10; i++){
+           
+                sum =sum+ 2;
+                System.out.println(sum);
+          
+                
+            }
+    }
+
+    
+}
--- loops.java
+++ loops.java
@@ -1,7 +1,23 @@
-public class loops {
+// public class loops {
+//     public static void main(String [] args){
+    
+//         for(int i =1; i<=5; i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+//---- using array of integers with loop---------
+
+public class loops{
     public static void main(String [] args){
-        for(int i =1; i<=5; i++){
-            System.out.println(i);
-        }
-    }
+        int [] arr = {1,2,6,9,3,5};
+        //for(int i=0; i<arr.length; i++)
+        //instead of this for loop try 
+        for(int x: arr)
+        {
+        System.out.println(x);
+     }}
 }
+     
+
